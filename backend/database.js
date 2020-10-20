64c169205e7978644d7e21ba2bf4011474259dfe
Billy Rodríguez <billyvecuss.com>
2020-10-20 11:54:11 -0500

    Set Database

--- backend/database.js
+++ backend/database.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const mongoose = require('mongoose') // Libreria para conectar a MongoDB
 // Acceder a variables de entorno
 process.env.MONGODB_URI
-
+const URI = `mongodb+srv://billy:${process.env.PASSWORD}@cluster0.deoom.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
 // Conexión a mongoDB, a través del protocolo de mongoDB, conectate al localhost y conecta al nombre de la db colocada
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true // Objeto para que no de alertas en terminal
 })
